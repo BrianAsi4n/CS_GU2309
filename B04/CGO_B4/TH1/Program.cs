10a81a24870258b69f447cd82805ed6a4aaab6ef
BrianAsi4n <146366610+BrianAsi4n@users.noreply.github.com>
2023-10-09 08:18:11 +0700

    baitapTH
    baiTap

---
+++ B04/CGO_B4/TH1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TH1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime localDate = DateTime.Now;
+            System.Console.WriteLine("DateTime now is: " + localDate);
+        }
+    }
+}
