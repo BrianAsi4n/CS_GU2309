9eeb6f1524485c8759f1c2f89866400006f931be
BrianAsi4n <146366610+BrianAsi4n@users.noreply.github.com>
2023-10-11 09:39:20 +0700

    bt
    sua lainbt

--- B05/CGO_05/BT1/Program.cs
+++ B05/CGO_05/BT1/Program.cs
@@ -10,36 +10,110 @@ namespace BT1
     {
         static void Main(string[] args)
         {
-            int height, weight;
-
-            Console.WriteLine("nhap height: ");
-            if (TryReadInteger(out height) && IsInRange(height, 0, 999))
+            Console.WriteLine("Nhập số cần đọc (0-999): ");
+            if (int.TryParse(Console.ReadLine(), out int number) && number >= 0 && number <= 999)
             {
-                Console.WriteLine("Nhập cân ưeight: ");
-                if (TryReadInteger(out weight) && IsInRange(weight, 0, 999))
+                string result = "";
+
+                if (number < 10)
                 {
-                    Console.WriteLine($"Chieu cao: {height} cm");
-                    Console.WriteLine($"can nang: {weight} kg");
+                    result = ReadOnes(number);
                 }
-                else
+                else if (number < 20)
                 {
-                    Console.WriteLine("can nang ko hop le ");
+                    result = ReadTeens(number);
                 }
+                else if (number < 100)
+                {
+                    int tens = number / 10;
+                    int ones = number % 10;
+                    result = ReadTens(tens) + " " + ReadOnes(ones);
+                }
+                else if (number <= 999)
+                {
+                    int hundreds = number / 100;
+                    int remainder = number % 100;
+
+                    result = ReadOnes(hundreds) + " hundred";
+
+                    if (remainder > 0)
+                    {
+                        result += " and ";
+                        if (remainder < 10)
+                        {
+                            result += ReadOnes(remainder);
+                        }
+                        else if (remainder < 20)
+                        {
+                            result += ReadTeens(remainder);
+                        }
+                        else
+                        {
+                            int tens = remainder / 10;
+                            int ones = remainder % 10;
+                            result += ReadTens(tens) + " " + ReadOnes(ones);
+                        }
+                    }
+                }
+
+                Console.WriteLine("Số bạn đã nhập đọc là: " + result);
             }
             else
             {
-                Console.WriteLine("Chieu cao ko hop le");
+                Console.WriteLine("Số không hợp lệ. Hãy nhập số từ 0 đến 999.");
             }
         }
 
-        static bool TryReadInteger(out int result)
+        static string ReadOnes(int number)
         {
-            return int.TryParse(Console.ReadLine(), out result);
+            switch (number)
+            {
+                case 0: return "zero";
+                case 1: return "one";
+                case 2: return "two";
+                case 3: return "three";
+                case 4: return "four";
+                case 5: return "five";
+                case 6: return "six";
+                case 7: return "seven";
+                case 8: return "eight";
+                case 9: return "nine";
+                default: return "";
+            }
         }
 
-        static bool IsInRange(int value, int min, int max)
+        static string ReadTeens(int number)
         {
-            return value >= min && value <= max;
+            switch (number)
+            {
+                case 10: return "ten";
+                case 11: return "eleven";
+                case 12: return "twelve";
+                case 13: return "thirteen";
+                case 14: return "fourteen";
+                case 15: return "fifteen";
+                case 16: return "sixteen";
+                case 17: return "seventeen";
+                case 18: return "eighteen";
+                case 19: return "nineteen";
+                default: return "";
+            }
+        }
+
+        static string ReadTens(int number)
+        {
+            switch (number)
+            {
+                case 2: return "twenty";
+                case 3: return "thirty";
+                case 4: return "forty";
+                case 5: return "fifty";
+                case 6: return "sixty";
+                case 7: return "seventy";
+                case 8: return "eighty";
+                case 9: return "ninety";
+                default: return "";
+            }
         }
     }
     
