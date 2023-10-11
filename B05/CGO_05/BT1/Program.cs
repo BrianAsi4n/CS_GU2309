9643351005f880e60db8ad269ed83e6bdf503250
BrianAsi4n <146366610+BrianAsi4n@users.noreply.github.com>
2023-10-11 09:21:24 +0700

    th,bt
    codegym

---
+++ B05/CGO_05/BT1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int height, weight;
+
+            Console.WriteLine("nhap height: ");
+            if (TryReadInteger(out height) && IsInRange(height, 0, 999))
+            {
+                Console.WriteLine("Nhập cân ưeight: ");
+                if (TryReadInteger(out weight) && IsInRange(weight, 0, 999))
+                {
+                    Console.WriteLine($"Chieu cao: {height} cm");
+                    Console.WriteLine($"can nang: {weight} kg");
+                }
+                else
+                {
+                    Console.WriteLine("can nang ko hop le ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Chieu cao ko hop le");
+            }
+        }
+
+        static bool TryReadInteger(out int result)
+        {
+            return int.TryParse(Console.ReadLine(), out result);
+        }
+
+        static bool IsInRange(int value, int min, int max)
+        {
+            return value >= min && value <= max;
+        }
+    }
+    
+}
---
+++ B05/CGO_05/CGO_05/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGO_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dayOfWeek = 6;
+
+            if(dayOfWeek <= 5)
+            {
+                Console.WriteLine("I have to go to work!");
+            }
+            else
+            {
+                Console.WriteLine("weekend");
+            }
+        }
+    }
+}
